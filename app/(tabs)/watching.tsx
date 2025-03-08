7a9224f04429df15ccd8a9b00138309c5b55fd46
Kevin Thumim <kevin.j.thum@gmail.com>
2025-03-08 17:17:18 -0500

    setup appwrite config

--- app/(tabs)/watching.tsx
+++ app/(tabs)/watching.tsx
@@ -1,11 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Watching = () => {
   return (
-    <View>
-      <Text>Watching Page</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        keyExtractor={(item: any) => item.id}
+        renderItem={({ item }) => (
+          <Text className="text-3xl text-white">{item.id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Watching Page
+                </Text>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Shows
+                </Text>
+              </View>
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,21 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "react-native-url-polyfill/auto";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl">In The Queue</Text>
-      <Link href="/watching" style={{ color: "blue" }}>
-        Go To Watching
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="flex-1 items-center justify-center min-h-[85vh]">
+          <Text className="text-3xl text-white font-bold">In The Queue</Text>
+          <Link href="/watching" style={{ color: "blue" }}>
+            Go To Watching
+          </Link>
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
