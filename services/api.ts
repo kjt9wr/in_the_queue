020456318f841565c1b3812fbd65a41b59dd4d6d
Kevin Thumim <kevin.j.thum@gmail.com>
2025-03-10 23:11:04 -0400

    create post request to add a show to the watchlist (queue)

--- services/api.ts
+++ services/api.ts
@@ -3,6 +3,7 @@ export const TMDB_CONFIG = {
   API_KEY: process.env.EXPO_PUBLIC_TV_API_KEY,
   headers: {
     accept: "application/json",
+    "content-type": "application/json",
     Authorization: `Bearer ${process.env.EXPO_PUBLIC_TV_API_KEY}`,
   },
 };
@@ -26,16 +27,23 @@ export const fetchShows = async ({ query }: { query: string }) => {
   return data.results;
 };
 
-// const url = 'https://api.themoviedb.org/3/account/21870676/watchlist/tv?language=en-US&page=1&sort_by=created_at.asc';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZmFhZThjZjMyZmUzMzc0Nzk0MzBjY2E1YjM2MzZmMCIsIm5iZiI6MTc0MTU3NzExNC4wMTksInN1YiI6IjY3Y2U1YjlhYWQ0ODZiNDNlYmUyZjUyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fRr8autO39X9AwYnaorewGTwsAJsQGd_qrei_jDWVE0'
-//   }
-// };
+export const addToWatchList = async (showId: number, isAdding: boolean) => {
+  const endpoint = `${TMDB_CONFIG.BASE_URL}/account/${process.env.EXPO_PUBLIC_ACCOUNT_ID}/watchlist`;
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: TMDB_CONFIG.headers,
+    body: JSON.stringify({
+      media_type: "tv",
+      media_id: showId,
+      watchlist: isAdding,
+    }),
+  });
 
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error("Failed to add show to list", response.statusText);
+  }
+  const data = await response.json();
+
+  return data.results;
+};
