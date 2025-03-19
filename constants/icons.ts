8075b3dc88a2e6e305f7a20ddb96cc70b696350d
Kevin Thumim <kevin.j.thum@gmail.com>
2025-03-19 19:42:27 -0400

    removed unused assets

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,31 +1,23 @@
-import arrow from "@/assets/icons/arrow.png";
+import check from "@/assets/icons/check.png";
 import enqueue from "@/assets/icons/enqueue.png";
 import finished from "@/assets/icons/finished.png";
-import home from "@/assets/icons/home.png";
-import person from "@/assets/icons/person.png";
 import play from "@/assets/icons/play.png";
 import queue from "@/assets/icons/queue.png";
 import logo from "@/assets/icons/queueIcon2.png";
-import save from "@/assets/icons/save.png";
 import search from "@/assets/icons/search.png";
 import star from "@/assets/icons/star.png";
 import timer from "@/assets/icons/timer.png";
 import tv from "@/assets/icons/tv.png";
-import check from "@/assets/icons/check.png";
 
 export const icons = {
-  home,
-  search,
-  person,
-  save,
-  star,
-  play,
-  arrow,
-  queue,
+  check,
+  enqueue,
   finished,
+  logo,
+  play,
+  search,
+  star,
+  queue,
   timer,
   tv,
-  logo,
-  enqueue,
-  check,
 };
