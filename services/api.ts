3c0c8b4c6a4fd20b0c89bf1465f8d0e47cd9ee39
Kevin Thumim <kevin.j.thum@gmail.com>
2025-03-20 17:10:58 -0400

    update adaptive icon

--- services/api.ts
+++ services/api.ts
@@ -13,9 +13,9 @@ export const TMDB_CONFIG = {
 };
 
 export const fetchShows = async ({ query }: { query: string }) => {
-  const endpoint = query
-    ? `${TMDB_CONFIG.BASE_URL}/search/tv?query=${encodeURIComponent(query)}`
-    : `${TMDB_CONFIG.BASE_URL}/account/${process.env.EXPO_PUBLIC_ACCOUNT_ID}/watchlist/tv?language=en-US&page=1&sort_by=created_at.asc`;
+  const endpoint = `${
+    TMDB_CONFIG.BASE_URL
+  }/search/tv?query=${encodeURIComponent(query)}`;
 
   const response = await fetch(endpoint, {
     method: "GET",
@@ -25,57 +25,6 @@ export const fetchShows = async ({ query }: { query: string }) => {
   if (!response.ok) {
     // @ts-ignore
     throw new Error("Failed to fetch shows", response.statusText);
-  }
-
-  const data = await response.json();
-  return data.results;
-};
-
-export const fetchAllShows = async () => {
-  const endpoint = `${TMDB_CONFIG.BASE_URL_V4}/list/${process.env.EXPO_PUBLIC_LIST_ID}`;
-  const response = await fetch(endpoint, {
-    method: "GET",
-    headers: TMDB_CONFIG.headers,
-  });
-
-  if (!response.ok) {
-    // @ts-ignore
-    throw new Error("Failed to fetch shows", response.statusText);
-  }
-
-  const data = await response.json();
-  return data.results;
-};
-
-export const addToTMDB = async (
-  list: string, // either "watchlist" or "favorite"
-  showId: number,
-  isAdding: boolean
-) => {
-  const endpoint = `${TMDB_CONFIG.BASE_URL}/account/${process.env.EXPO_PUBLIC_ACCOUNT_ID}/${list}`;
-
-  const requestBodyParams =
-    list === "watchlist"
-      ? {
-          watchlist: isAdding,
-        }
-      : {
-          favorite: isAdding,
-        };
-
-  const response = await fetch(endpoint, {
-    method: "POST",
-    headers: TMDB_CONFIG.headers,
-    body: JSON.stringify({
-      ...requestBodyParams,
-      media_type: "tv",
-      media_id: showId,
-    }),
-  });
-
-  if (!response.ok) {
-    // @ts-ignore
-    throw new Error("Failed to add show to list", response.statusText);
   }
 
   const data = await response.json();
